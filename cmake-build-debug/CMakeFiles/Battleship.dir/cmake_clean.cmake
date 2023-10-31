92a13063ebc74cc00c3ca4b94b6da094d16978d1
StaZyXx <samuel.letellier@supinfo.Com>
2023-10-31 09:21:04 +0100

    Add cmake to repo

---
+++ cmake-build-debug/CMakeFiles/Battleship.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "Battleship.exe"
+  "Battleship.exe.manifest"
+  "Battleship.pdb"
+  "CMakeFiles/Battleship.dir/cpp/Case.cpp.obj"
+  "CMakeFiles/Battleship.dir/cpp/Case.cpp.obj.d"
+  "CMakeFiles/Battleship.dir/cpp/Game.cpp.obj"
+  "CMakeFiles/Battleship.dir/cpp/Game.cpp.obj.d"
+  "CMakeFiles/Battleship.dir/cpp/Player.cpp.obj"
+  "CMakeFiles/Battleship.dir/cpp/Player.cpp.obj.d"
+  "CMakeFiles/Battleship.dir/cpp/Ship.cpp.obj"
+  "CMakeFiles/Battleship.dir/cpp/Ship.cpp.obj.d"
+  "CMakeFiles/Battleship.dir/main.cpp.obj"
+  "CMakeFiles/Battleship.dir/main.cpp.obj.d"
+  "libBattleship.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Battleship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
